5a487657f55262ff16fe3e5d7d0b46f7e3c930de
KeerthiChandraC <keerthichand.c@gmail.com>
2022-05-24 13:48:28 +0530

    commit

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ TEST_URL: str = f"file://{FILE_PATH}/myurl.html"
 URL: str =  "https://www.sbtet.telangana.gov.in/index.html#!/index/StudentFeedback"
 FB_URL : str = "https://www.sbtet.telangana.gov.in/API/api/PreExamination/GenerateOtpForFeedback?FeedbackId=3&Pin=21087-EC-059"
 FB_PIN_URL : str = "https://www.sbtet.telangana.gov.in/API/api/PreExamination/getFacultyData?FeedbackId=3&Otp=5E5214&Pin=21087-EC-059"
+
+
 def main()-> None:
     sb_fdback = init_driver()
     sb_fdback.get(URL)
@@ -38,12 +40,12 @@ def main()-> None:
         box = sb_fdback.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="feedbackcontainer"]/form/div[3]/table/tbody')))
         #stars = box.find_element_by_css_selector("[title^='Excellent']")
         #rating = box.find_elements(by=By.CLASS_NAME, value='rating')
-        sleep(5)
+        sleep(3)
         stars = box.find_elements(by=By.CSS_SELECTOR, value="[title^='Excellent']")
-        print("found stars")
+        print("found stars...")
         #stars = box.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, value="[title^='Excellent']"))
+        print("clicking satrs...")
         for star in stars:
-            print("clicking satrs")
             star.click()
         print("Clik sumbit")
         #sub.click()
