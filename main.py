5265b6f00deb927f29f8e6ae9f27821e8ac36aa9
KeerthiChandraC <91390711+KeerthiChandraC@users.noreply.github.com>
2023-09-30 17:14:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 #from selenium.common.exceptions import TimeoutException
 #from selenium.webdriver.support.ui import Select
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import os
 import configparser
